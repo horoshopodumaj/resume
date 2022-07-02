3cb16d763430f761a09aa2a094f132a7c08d586e
Diana Murtazina <murka8831@yandex.ru>
2022-07-02 23:20:23 +0400

    typed.js plugin has been added, added a description to the about section

---
+++ js/index.js
@@ -0,0 +1,8 @@
+let options = {
+    strings: ["a junior front-end developer.", "&amp; a second sentence."],
+    typeSpeed: 40,
+    backSpeed: 20,
+    loop: true,
+};
+
+let typed = new Typed(".element", options);
