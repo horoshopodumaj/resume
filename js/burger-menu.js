aa0a9c1b7ec634bfa13887a3b35fe115dfbeb5eb
Diana Murtazina <murka8831@yandex.ru>
2022-07-07 13:00:03 +0400

    added burger menu

---
+++ js/burger-menu.js
@@ -0,0 +1,28 @@
+const menu = document.getElementById("burger-menu");
+const menuClose = document.getElementById("burger-menu-close");
+const menuOpen = document.getElementById("burger-menu-open");
+const menuCloseO = document.querySelector("burger-menu__menu");
+
+if (menuOpen) {
+    menuOpen.addEventListener("click", function () {
+        if (menu) {
+            menu.setAttribute("data-open", "");
+        }
+    });
+}
+
+if (menuClose) {
+    menuClose.addEventListener("click", function () {
+        if (menu) {
+            menu.removeAttribute("data-open");
+        }
+    });
+}
+
+if (menuCloseO) {
+    menuCloseO.addEventListener("click", function () {
+        if (menu) {
+            menu.removeAttribute("data-open");
+        }
+    });
+}
