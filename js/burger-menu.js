058788b06ed8b6bf4275e65109660f3be8d654fb
Diana Murtazina <murka8831@yandex.ru>
2022-07-08 16:49:04 +0400

    Closing the burger menu when selecting the section to go to, added scrolling burger menu with a large number of items, added a ban on scrolling the main page when the burger menu is open

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,12 +1,13 @@
 const menu = document.getElementById("burger-menu");
 const menuClose = document.getElementById("burger-menu-close");
 const menuOpen = document.getElementById("burger-menu-open");
-const menuCloseO = document.querySelector("burger-menu__menu");
+const menuBody = document.querySelector(".burger-menu__menu");
 
 if (menuOpen) {
     menuOpen.addEventListener("click", function () {
         if (menu) {
             menu.setAttribute("data-open", "");
+            document.body.classList.add("_lock");
         }
     });
 }
@@ -15,14 +16,14 @@ if (menuClose) {
     menuClose.addEventListener("click", function () {
         if (menu) {
             menu.removeAttribute("data-open");
+            document.body.classList.toggle("_lock");
         }
     });
 }
 
-if (menuCloseO) {
-    menuCloseO.addEventListener("click", function () {
-        if (menu) {
-            menu.removeAttribute("data-open");
-        }
+if (menuBody) {
+    menuBody.addEventListener("click", function () {
+        menu.removeAttribute("data-open");
+        document.body.classList.toggle("_lock");
     });
 }
