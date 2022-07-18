8fd42ba3a4a58c84248069b35719bb27136a38ff
Diana Murtazina <murka8831@yandex.ru>
2022-07-18 15:59:31 +0400

    added the ability to switch to Russian

---
+++ js/app.js
@@ -0,0 +1,30 @@
+let select = document.querySelector("select");
+let allLang = ["en", "ru"];
+
+select.addEventListener("change", changeUrlLanguage);
+
+function changeUrlLanguage() {
+    let lang = select.value;
+    location.href = window.location.pathname + "#" + lang;
+    location.reload();
+}
+
+function changeLanguage() {
+    let hash = window.location.hash;
+    hash = hash.substring(1);
+    console.log(hash);
+    if (!allLang.includes(hash)) {
+        location.href = window.location.pathname + "#en";
+        location.reload();
+    }
+
+    select.value = hash;
+    for (let key in langArr) {
+        let elem = document.querySelector(".lng-" + key);
+        if (elem) {
+            elem.innerHTML = langArr[key][hash];
+        }
+    }
+}
+
+changeLanguage();
---
+++ js/lang.js
@@ -0,0 +1,140 @@
+const langArr = {
+    home: {
+        ru: "Главная",
+        en: "Home",
+    },
+    about: {
+        ru: "Обо мне",
+        en: "About me",
+    },
+    projects: {
+        ru: "Мои проекты",
+        en: "My projects",
+    },
+    contact: {
+        ru: "Мои контакты",
+        en: "Contact me",
+    },
+
+    resume: {
+        ru: "Резюме",
+        en: "Resume",
+    },
+
+    hi: {
+        ru: "Привет, меня зовут",
+        en: "Hi, my name is",
+    },
+    name: {
+        ru: "Муртазина Диана",
+        en: "Diana Murtazina",
+    },
+
+    aboutme: {
+        ru: "Обо мне",
+        en: "About me",
+    },
+    aboutmetext: {
+        ru: `Привет, меня зовут Муртазина Диана и я только начинаю свою
+        карьеру front-end разработчика. В течении нескольких месяцев я
+        изучала HTML, CSS, JavaScript, алгоритмы и структуры данных, совмещая учебу
+        и работу руководителем направления в отделе физической защиты наземных и 
+        морских объектов. На данный момент я изучаю React.`,
+        en: `Hi, my name is Diana Murtazina and I am novice
+        front-end developer who is building a new career
+        from the thing I really enjoy. For several
+        months, I have been honing my web developer
+        skills by studying HTML, CSS, JavaScript,
+        algorithms and data structures, while
+        simultaneously working as a head of direction in
+        the department of physical protection of land
+        and sea objects. Now I'm studying React.`,
+    },
+
+    myprojects: {
+        ru: "Мои проекты",
+        en: "My projects",
+    },
+
+    landing: {
+        ru: "Верстка лендинга",
+        en: "Landing page layout",
+    },
+
+    landingtext: {
+        ru: `Верстка лендинга для языковой школы. 
+        Сайт сверстан с применением методологии БЭМ, scss, postcss. Изображения в проекте вставляются из svg спрайта.`,
+        en: `Layout of the landing site according to
+        the layout for a language school. The
+        project uses BEM, scss, postcss. Images in the project are inserted from svg-sprite.`,
+    },
+
+    reus: {
+        ru: "Библиотека переиспользуемых компонентов",
+        en: "Reusable component library",
+    },
+
+    reustext: {
+        ru: `Проект был выполнен в рамках курса по современной верстке.
+        Это библиотека переиспользуемых компонентов, таких как:
+        модальное окно, элементы форм, календарь, гистограмма, скелетон и другие.
+        Дизайн сайта адаптивен. В проекте использована методология БЭМ, scss. Каждый компонент
+        представлен на своей странице. Есть возможность сменить тему на темную.`,
+        en: `The project was completed as part of a
+        course on modern layout. It is a library
+        of reusable components, such as a modal
+        window, form elements, a calendar, a
+        histogram, a skeleton, and others. The
+        layout is adapted for mobile, tablet and
+        desktop devices. The project uses BEM
+        methodology, scss preprocessor. Each
+        component is presented on a separate
+        page, it is possible to include a dark
+        theme.`,
+    },
+
+    restaurant: {
+        ru: "Сайт ресторана",
+        en: "Restaurant website",
+    },
+
+    restauranttext: {
+        ru: `Ресторан с онлайн-заказом "Bangkok Express". Проект сделан по 
+        готовой верстке. С помощью javascript были реализованы: карусель товаров
+        в верхней части сайта, ленточное меню, слайдер, подгрузка товаров с сервера, 
+        динамическая фильтрация списка товаров. А также корзина товаров с динамическим 
+        позиционированием при прокрутке, анимацией, модальным окном и формой заказа. 
+        Компонентная архитектура позволяет легко расширять ресторан, усложнять интерфейс и структуру.`,
+        en: `Restaurant with online order "Bangkok
+        Express". The project uses a ready-made
+        layout. The use of javascript in the
+        project made it possible to implement: a
+        carousel with goods at the top of the
+        site, a ribbon menu, a slider, loading
+        goods from the server, dynamic filtering
+        of the list of goods. As well as a
+        basket of goods with dynamic scrolling
+        positioning, animation, modal window and
+        order form. The component architecture
+        makes it easy to expand the restaurant,
+        complicate the interface and structure.`,
+    },
+
+    contactme: {
+        ru: "Связаться со мной",
+        en: "Contact me",
+    },
+
+    opportunities: {
+        ru: `В настоящее время я ищу возможности, вы можете написать мне с предложением работы
+        или просто поговорить. Я отвечу на ваше письмо.`,
+        en: `I'm currently looking for opportunities, my inbox is
+        always open whether for hiring me or just to talk.
+        I'll answer your email!`,
+    },
+
+    writeforme: {
+        ru: "Моя почта здесь",
+        en: "Let's get in touch",
+    },
+};
