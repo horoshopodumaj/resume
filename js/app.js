45b9389cff639612d8bf1c8d336ea91f6c380bed
Diana Murtazina <murka8831@yandex.ru>
2022-10-18 20:25:58 +0400

    fixed problems with the same id

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 let select = document.querySelector("#select");
+let selectBurger = document.querySelector("#select_burger");
 let allLang = ["en", "ru"];
 
 select.addEventListener("change", changeUrlLanguage);
+selectBurger.addEventListener("change", changeUrlLanguageBurger);
 
 function changeUrlLanguage() {
     let lang = select.value;
@@ -10,11 +12,16 @@ function changeUrlLanguage() {
     location.reload();
     console.log(lang);
 }
+function changeUrlLanguageBurger() {
+    let lang = selectBurger.value;
+
+    location.href = window.location.pathname + "#" + lang;
+    location.reload();
+}
 
 function changeLanguage() {
     let hash = window.location.hash;
     hash = hash.substring(1);
-    console.log(hash);
     if (!allLang.includes(hash)) {
         location.href = window.location.pathname + "/";
         location.reload();
@@ -28,5 +35,22 @@ function changeLanguage() {
         }
     }
 }
+function changeLanguageBurger() {
+    let hash = window.location.hash;
+    hash = hash.substring(1);
+    if (!allLang.includes(hash)) {
+        location.href = window.location.pathname + "/";
+        location.reload();
+    }
+
+    selectBurger.value = hash;
+    for (let key in langArr) {
+        let elem = document.querySelector(".lng-" + key);
+        if (elem) {
+            elem.innerHTML = langArr[key][hash];
+        }
+    }
+}
 
 changeLanguage();
+changeLanguageBurger();
